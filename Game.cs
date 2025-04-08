3a9e8d8c8f38fa91cdd579f312885d4c60d29894
aquatic_animal <angelocaceda333@gmail.com>
2025-04-08 17:11:41 +0200

    Ejercicio2
    RAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Game.cs
+++ Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,46 @@ namespace POO
     {
         static void Main(string[] args)
         {
+            List<Pokemon> pokeList = new List<Pokemon>();
             Bulbasaur bulbasaur = new Bulbasaur();
-            bulbasaur.PokemonShout();
             Stantler stantler = new Stantler();
-            stantler.PokemonShout();
+            Simisear simisear = new Simisear();
+            Miraidon miraidon = new Miraidon();
+            pokeList.Add(bulbasaur);
+            pokeList.Add(miraidon);
+            pokeList.Add(stantler);
+            pokeList.Add(simisear);
+            List<Pokemon> catchedPokemons = new List<Pokemon>();
+            bool gameRunning = true;
+            while (gameRunning)
+            {
+                Random randSpawn = new Random();
+                int newSpawn = randSpawn.Next(0, pokeList.Count());
+                Console.WriteLine("Un nuevo pokemon ha aparecido: " + pokeList[newSpawn]);
+                Console.WriteLine("Presiona la tecla <0> si quieres intentar capturar el pokemon.");
+                Console.WriteLine("Presiona la tecla <1> si quieres dejar escapar al pokemon.");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 1)
+                {
+                    Console.WriteLine("Has dejado huir al pokemon.");
+                }
+                else if (answer == 0)
+                {
+                    Random catchChance = new Random();
+                    int result = catchChance.Next(0, 10);
+                    if (result <= 4)
+                    {
+                        Console.WriteLine("El pokemon se ha escapado. Mala suerte.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Has logrado capturar al pokemon! " + pokeList[newSpawn]);
+
+                    }
+                }
+            }
+
+
         }
     }
 }
---
+++ Miraidon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Miraidon : Pokemon
+    {
+        public Miraidon()
+        {
+            Name = "Miraidon";
+            Type = "Dragon";
+            Health = 100;
+        }
+    }
+}
